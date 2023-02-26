e925cc98d04ded417d6665f06050267c6e894cc9
tolujacob05 <tolujacob05@gmail.com>
2023-02-26 23:09:00 +0100

    front-page done

---
+++ publc/JS/nav.js
@@ -0,0 +1,26 @@
+const createNav = () => {
+    let nav = document.querySelector(".navbar");
+
+    nav.innerHTML = `
+        <div class="nav">
+            <h1>LOGO</h1>
+            <div class="nav-items">
+                <div class="search">
+                    <input type="text" class="search-box" placeholder="search brand, product">
+                    <button class="search-btn">Search</button>
+                </div>
+                <a href="#"><img src="img/user.png" alt=""></a>
+                <a href="#"><img src="img/cart.png" alt=""></a>
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">Home</a></li>
+            <li class="link-item"><a href="#" class="link">Women</a></li>
+            <li class="link-item"><a href="#" class="link">Men</a></li>
+            <li class="link-item"><a href="#" class="link">Kids</a></li>
+            <li class="link-item"><a href="#" class="link">Accessories</a></li>
+        </ul>
+    `;
+}
+
+createNav();
